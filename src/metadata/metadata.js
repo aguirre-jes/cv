d942dfb2281b8fbe3af600e16aff0b0943312b11
Jesus Aguirre <info.aguirre.jesus@gmail.com>
2023-01-16 18:37:58 -0500

    Update medatada.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -26,8 +26,8 @@ module.exports = {
       skills: ['OCI', 'Twelve Factors App', 'Design Patterns', 'Microservices', 'Java'],
       contents: `[Indra](https://www.indracompany.com/es) es una de las principales compañías globales de tecnología y consultoría y el socio tecnológico para las operaciones clave de los negocios de sus clientes en todo el mundo.\n
 Como arquitecto de software, soy responsable de:\n
-*XYZ
-*ZXY`
+* XYZ
+* ZXY`
     },
     {
       title: 'Twelfth Night',
