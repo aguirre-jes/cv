869ae44141a73ad751af393029f6127b9dfcdf62
Jesus Aguirre <info.aguirre.jesus@gmail.com>
2023-01-17 15:08:23 -0500

    Update information for template

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       title: 'Indra - Minsait - PA',
       period: 'Oct 2020 - Presente',
-      skills: ['OCI', 'Twelve Factors App', 'Design Patterns', 'Microservices', 'Java'],
+      skills: ['OCI', 'AWS', 'Twelve Factors App', 'Design Patterns', 'Monitoring', 'Tracing', 'Microservices', 'JavaScript', 'Java'],
       contents: `Software Architect\n
 [Indra](https://www.indracompany.com/es) es una de las principales compañías globales de tecnología y consultoría y el socio tecnológico para las operaciones clave de los negocios de sus clientes en todo el mundo.\n
 Como arquitecto de software, soy responsable de:\n
